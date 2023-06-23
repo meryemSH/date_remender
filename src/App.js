d5e587faad1562c35ed1ca14b87b13b984adf94c
meryem SH <meryem2003020@gmail.com>
2023-06-23 20:12:04 +0100

    add style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import logo from './logo.svg';
+
+import { Col, Container, Row } from 'react-bootstrap';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-<h1>meryem</h1>
+    <div className="font color-body">
+
+        <Container className='py-5'>
+
+          <Row className='justify-content-center'>
+            <Col sm='8' >
+            Vous avez 4 rendez-vous aujourd'hui
+            </Col>
+          </Row>
+
+
+          <Row className='justify-content-center'>
+            <Col sm='8' >
+            <div className='rectangle'>
+              
+            </div>
+            </Col>
+          </Row>
+
+
+          <Row className='justify-content-center my-2 '>
+            <Col sm='8' className='d-flex justify-content-between'>
+              <button className='btn-style p-1'>Supprimer Tout</button>
+              <button className='btn-style p-1'>Afficher Tout</button>
+            </Col>
+          </Row>
+        </Container>
 
     </div>
   );
